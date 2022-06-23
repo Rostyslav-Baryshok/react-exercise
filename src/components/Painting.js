8b894f08f36c2bd4761dce170740106f979c2490
Rostyslav-Baryshok <85187900+Rostyslav-Baryshok@users.noreply.github.com>
2022-06-23 20:20:25 +0300

    Painting.js

---
+++ src/components/Painting.js
@@ -0,0 +1,14 @@
+export default function Painting({ url, title, profileUrl, author, price }) {
+  return (
+    <div>
+      <img src={url} alt={title} width="480" />
+      <h2>{title}</h2>
+      <p>
+        Автор: <a href={profileUrl}>{author}</a>
+      </p>
+      <p>Цена: {price} кредитов</p>
+      <p>Доступность: заканчивается или есть в наличии</p>
+      <button type="button">Добавить в корзину</button>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,19 +1,4 @@
 import ReactDOM from 'react-dom';
-import paintings from '../src/paintings.json';
+import App from './/components/App';
 
-const data = paintings[0];
-
-const painting = (
-  <div>
-    <img src={data.url} alt={data.title} width="480" />
-    <h2>{data.title}</h2>
-    <p>
-      Автор: <a href={data.author.url}>{data.author.tag}</a>
-    </p>
-    <p>Цена: {data.price} кредитов</p>
-    <p>Доступность: заканчивается или есть в наличии</p>
-    <button type="button">Добавить в корзину</button>
-  </div>
-);
-
-ReactDOM.render(painting, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector('#root'));
