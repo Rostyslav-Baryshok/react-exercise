5b8255e6bc2dab1c2f5192908f46325cf43d1234
Rostyslav-Baryshok <85187900+Rostyslav-Baryshok@users.noreply.github.com>
2022-06-24 11:36:03 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,32 +4,17 @@ import paintings from '../paintings.json';
 export default function App() {
   return (
     <div>
-      <Painting
-        imageUrl={paintings[0].url}
-        title={paintings[0].title}
-        author={paintings[0].author.tag}
-        profileUrl={paintings[0].author.url}
-        price={paintings[0].price}
-        quantity={paintings[0].quantity}
-      />
-
-      <Painting
-        imageUrl={paintings[1].url}
-        title={paintings[1].title}
-        author={paintings[1].author.tag}
-        profileUrl={paintings[1].author.url}
-        price={paintings[1].price}
-        quantity={paintings[1].quantity}
-      />
-
-      <Painting
-        imageUrl={paintings[2].url}
-        title={paintings[2].title}
-        author={paintings[2].author.tag}
-        profileUrl={paintings[2].author.url}
-        price={paintings[2].price}
-        quantity={paintings[2].quantity}
-      />
+      {paintings.map(painting => (
+        <Painting
+          key={painting.id}
+          imageUrl={painting.url}
+          title={painting.title}
+          author={painting.author.tag}
+          profileUrl={painting.author.url}
+          price={painting.price}
+          quantity={painting.quantity}
+        />
+      ))}
     </div>
   );
 }
